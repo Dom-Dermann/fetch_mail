cdb679452ae1547e64d6adbad258f176fcfd9ead
Dom Dermann <dominikdermann@gmail.com>
2019-04-24 07:18:10 -0700

    script analyzes seed money and returns maximum

--- grab_attachments.py
+++ grab_attachments.py
@@ -1,5 +1,6 @@
 import mail_toolbox
 import argparse
+import graph_converter as gc
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Please provide the password.")
@@ -8,7 +9,16 @@ if __name__ == "__main__":
 
     gmail = mail_toolbox.FetchMail('imap.gmail.com', 'fileinputs@gmail.com', args.password)
     messages = gmail.fetch_unread_messages()
+
+    paths = []
     for i, msg in enumerate(messages):
         path = gmail.save_attachment(msg)
+        paths.append(path)
         print(f'File {i+1} written in {path}')
 
+    for path in paths:
+        dataset = gc.data_model(path)
+        se = dataset.find_max_money()
+        print(se)
+
+   
