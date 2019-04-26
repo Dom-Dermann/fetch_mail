58f7a523ff677d4af3961b65ae0f26deb28cbe98
Dominikus Dermann <dominikdermann@gmail.com>
2019-04-26 01:58:59 -0700

    move files to desired location (Linux only)

---
+++ displace-files.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+## This bash script scans for keywords in files downloaded by the python script in this folder and allocates the files to appropriate
+## folders for further processing
+[[ "cat" == "cat"  ]]
+echo $?
